b77c8d35e095b1b208d2eaee5d74e3de50c4c6eb
Nathu Nandwani <nandwaninathu@yahoo.com>
2018-10-02 01:33:45 +0800

    Added url for usernames

--- app/src/main/java/workshop/android/cspcert/challenge/APIv2.java
+++ app/src/main/java/workshop/android/cspcert/challenge/APIv2.java
@@ -2,4 +2,5 @@ package workshop.android.cspcert.challenge;
 
 public class APIv2 {
     public static String url = "http://nandtech.co/cspcert-android-workshop/php/v2/login.php";
+    public static String usernames = "http://nandtech.co/cspcert-android-workshop/php/v2/users.php";
 }
