1f9bbd51f1bc2f090a54017f2b3b8106ee38de48
Nathu Nandwani <nandwaninathu@yahoo.com>
2018-10-02 14:05:40 +0800

    Fetching usernames from URL

--- app/src/main/java/workshop/android/cspcert/challenge/MainActivity.java
+++ app/src/main/java/workshop/android/cspcert/challenge/MainActivity.java
@@ -1,14 +1,20 @@
 package workshop.android.cspcert.challenge;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,6 +25,8 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String PLEASE_EXCUSE_MY_SLOPPY_CODE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setActivityBackgroundColor(0);
         SaveData();
         ImageActions();
-        DBGenerate();
+        new SendPostRequest().execute();
     }
 
     public void setActivityBackgroundColor(int color) {
@@ -86,8 +94,47 @@ public class MainActivity extends AppCompatActivity {
         DataHelper.SaveData(MainActivity.this, "challenges", dictionary, false);
     }
 
-    public void DBGenerate() {
-        DatabaseHelper db = new DatabaseHelper(this);
-        db.InsertAuthor("coco");
+    public class SendPostRequest extends AsyncTask<String, Void, String> {
+        protected void onPreExecute(){}
+        protected String doInBackground(String... arg0) {
+            try {
+                URL url = new URL(APIv2.usernames);
+
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                int responseCode = conn.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    StringBuffer sb = new StringBuffer("");
+                    String line = "";
+                    while((line = in.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+                    in.close();
+                    return sb.toString();
+                } else {
+                    return "false : "+responseCode;
+                }
+            }
+            catch(Exception e){
+                return "Exception: " + e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i("stats", "Entered DB Generate");
+            DatabaseHelper db = new DatabaseHelper(MainActivity.this);
+            String[] splitter = result.split("\\|");
+            for (int i = 0; i < splitter.length; i++) {
+                db.InsertAuthor(splitter[i]);
+            }
+        }
     }
 }
