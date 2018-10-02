b01f1f8b92a618e417d7535c008304615a1d90c5
Nathu Nandwani <nandwaninathu@yahoo.com>
2018-10-02 19:28:09 +0800

    Added flag in news

--- app/src/main/java/workshop/android/cspcert/challenge/MainActivity.java
+++ app/src/main/java/workshop/android/cspcert/challenge/MainActivity.java
@@ -12,6 +12,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
--- app/src/main/java/workshop/android/cspcert/challenge/NewsActivity.java
+++ app/src/main/java/workshop/android/cspcert/challenge/NewsActivity.java
@@ -2,6 +2,9 @@ package workshop.android.cspcert.challenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Gravity;
+import android.widget.TextView;
 
 public class NewsActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class NewsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        DatabaseHelper db = new DatabaseHelper(this);
+
+        News news = db.getNews(1);
+
+        TextView txtTitle = findViewById(R.id.txtTitle);
+        txtTitle.setText(news.getTitle());
+        txtTitle.setGravity(Gravity.CENTER);
+
+        TextView txtContents = findViewById(R.id.txtContents);
+        txtContents.setText(news.getContent());
+        txtContents.setGravity(Gravity.CENTER);
+        txtContents.setMovementMethod(new ScrollingMovementMethod());
     }
 }
